d1881fd6a11f243f96be14951a742c3d402c873a
Dankisey <greeng3n@gmail.com>
2021-11-20 15:00:29 +0300

    Update Finish.cs

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -3,15 +3,14 @@ using UnityEngine;
 public class Finish : MonoBehaviour
 {
     [SerializeField] private AudioClip _enterSound;
-    [SerializeField] private GameObject _audioPlayerPrefab;
+    [SerializeField] private AudioPlayer _audioPlayerPrefab;
 
     private AudioPlayer _audioPlayer;
     private bool _finished;
 
     private void Start()
     {
-        var audioPlayer = Instantiate(_audioPlayerPrefab);
-        _audioPlayer = audioPlayer.GetComponent<AudioPlayer>();
+        _audioPlayer = Instantiate(_audioPlayerPrefab);
         _finished = false;
     }
 
